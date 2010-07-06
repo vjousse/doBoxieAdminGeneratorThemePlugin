0be3dd1d077d994f8c0bf407e343161f1ad08630
Arturo Linares <arturo.linares@gmail.com>
2010-07-05 20:45:12 -0500

    Changed the position of the sort container

--- data/generator/sfDoctrineModule/boxieAdmin/template/templates/_list_th_tabular.php
+++ data/generator/sfDoctrineModule/boxieAdmin/template/templates/_list_th_tabular.php
@@ -3,10 +3,10 @@
 <th class="sf_admin_<?php echo strtolower($field->getType()) ?> sf_admin_list_th_<?php echo $name ?>">
 <?php if ($field->isReal()): ?>
   [?php if ('<?php echo $name ?>' == $sort[0]): ?]
-    [?php echo link_to(__('<?php echo $field->getConfig('label', '', true) ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), '@<?php echo $this->getUrlForAction('list') ?>', array('query_string' => 'sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?]
     <div class="sort_container">
         [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?]
     </div>
+    [?php echo link_to(__('<?php echo $field->getConfig('label', '', true) ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), '@<?php echo $this->getUrlForAction('list') ?>', array('query_string' => 'sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?]
   [?php else: ?]
     [?php echo link_to(__('<?php echo $field->getConfig('label', '', true) ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), '@<?php echo $this->getUrlForAction('list') ?>', array('query_string' => 'sort=<?php echo $name ?>&sort_type=asc')) ?]
   [?php endif; ?]

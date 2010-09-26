e01fe14f110ee5bec2c9c697679acb00f99f840b
Arturo Linares <arturo.linares@gmail.com>
2010-09-26 17:21:42 -0500

    Added support for more than one field per row in the admin generator.

--- data/generator/sfDoctrineModule/boxieAdmin/template/templates/_form_field.php
+++ data/generator/sfDoctrineModule/boxieAdmin/template/templates/_form_field.php
@@ -16,18 +16,20 @@
   [?php include_partial("$module/".$action, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 [?php elseif ($field->isComponent()): ?]
   [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
-[?php else: ?]
-    <label for="[?php echo $name ?]">
-        [?php if($form[$name]->getWidget() instanceof sfWidgetFormInputCheckbox): ?]
-          [?php $className='check'; ?]
-        [?php else: ?]
-          [?php $className=''; ?]
-        [?php endif; ?]
-        [?php echo $form[$name]->renderLabel($label,array('class'=>$className)) ?]
+[?php else:
+    if($form[$name]->getWidget() instanceof sfWidgetFormInputCheckbox):
+      $className='check';
+    else:
+      $className='';
+    endif;
 
-        [?php unset($className) ?]
-    </label>
-    [?php $new_classes = array() ?]
+    if ( $show_label ):
+        echo $form[$name]->renderLabel($label,array('class'=>$className));
+    endif;
+
+    unset($className);
+
+    $new_classes = array();?]
     [?php if ($form[$name]->hasError()): ?]
         [?php $new_classes = array('class' => 'error') ?]
     [?php endif ?]
@@ -39,6 +41,8 @@
     [?php else: ?]
         [?php echo $form[$name]->render($attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : array_merge($attributes, $new_classes)) ?]
     [?php endif; ?]
+
+    [?php if ( $show_help ): ?]
     <br>
     <small>
         [?php if ($help): ?]
@@ -47,4 +51,6 @@
           [?php echo $help ?]
         [?php endif; ?]
     </small>
+    [?php endif ?]
+
 [?php endif; ?]
